1ed877f92de663a03d3c1c901b6ce7e1349fc49d
Manoj Kumar <manojkumararumainathan@gmail.com>
2024-06-30 20:48:49 +0530

    Basic Java

---
+++ 7.Switch/Switch.java
@@ -0,0 +1,33 @@
+public class Switch {
+    public static void main(String[] args) {
+        
+        int n = 10;
+
+        switch (n) {
+            case 1:
+                System.out.println("Mon");
+                break;
+            case 2:
+                System.out.println("Tue");
+                break;
+            case 3:
+                System.out.println("Wed");
+                break;
+            case 4:
+                System.out.println("Thurs");
+                break;
+            case 5:
+                System.out.println("Fri");
+                break;
+            case 6:
+                System.out.println("Sat");
+                break;
+            case 7:
+                System.out.println("Sun");
+                break;
+            default:
+                System.out.println("Invalid Day");
+                break;
+        }
+    }    
+}
