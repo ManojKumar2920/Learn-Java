1ed39bd4ecc8bfcc1632ad16d005ef0388bb2eb1
Manoj Kumar <manojkumararumainathan@gmail.com>
2024-07-07 22:27:38 +0530

    Strings

---
+++ 18. Strings/Demo.java
@@ -0,0 +1,11 @@
+
+public class Demo {
+    public static void main(String[] args) {
+         
+        String name = "mano"; //String in java is a class and it will create a object in heap memory
+
+        // String name = new String("mano");
+
+        System.out.println(name);
+    }
+}
