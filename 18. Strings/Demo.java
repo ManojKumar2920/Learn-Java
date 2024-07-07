eec957c19152620c391abcd841ef0b03ffe65b39
Manoj Kumar <manojkumararumainathan@gmail.com>
2024-07-07 22:41:59 +0530

    String Constant Pool - Immutable concept

--- 18. Strings/Demo.java
+++ 18. Strings/Demo.java
@@ -7,5 +7,22 @@ public class Demo {
         // String name = new String("mano");
 
         System.out.println(name);
+
+        name = name + " swe";
+
+        System.out.println(name);
+
+        //strings in java are immutable. once the object is created in string constant pool, it will never get change but the name here is concatinated but it's create new obj with new add in scp.
+        
+
+        String name2 = "subu";
+        String name3 = "subu";
+        String name4 = "Subu";
+
+        System.out.println(name2.hashCode());
+        System.out.println(name3.hashCode());
+        System.out.println(name4.hashCode());
+
+        //the above line are the proven sample for strings are immutable
     }
 }
